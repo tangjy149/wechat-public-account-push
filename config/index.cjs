e252602bbafcfd9120f9e260043b391dc8d99c3d
tangjy149 <42775589+tangjy149@users.noreply.github.com>
2023-10-01 23:06:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obcz76ZjkxdcaN41iXTGBw-lxi3E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'GW8HatWX6ZFHCsVwnLlb4KDEbFRKjofyAQLIaxyJ_is',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-26',
       festivals: [
