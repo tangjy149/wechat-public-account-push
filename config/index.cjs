a2da6afb75123d7fc571ec7882f4ee1aec56ab1c
tangjy149 <42775589+tangjy149@users.noreply.github.com>
2023-10-12 22:44:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,20 +6,22 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  
+
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx39339dc7c2dd926e',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'e42f6ff9d1625e03bb0782f2123a8e44',
 
-  PROVINCE: '四川',
+  IS_SHOW_COLOR: true,
+
+  // PROVINCE: '成都',
   CITY: '成都',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝宝',
+      name: '玥玥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'obcz76ZjkxdcaN41iXTGBw-lxi3E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -27,41 +29,91 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-26',
       festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // {
+        //   type: '*生日', name: '婉婉阴历生日', year: '1996', date: '05-19',
+        // },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1997', date: '11-26',
+          type: '生日', name: '玥玥阳历生日', year: '1999', date: '11-26',
         },
         {
-          type: '节日', name: '开盒纪念日', year: '2022', date: '11-07',
+          type: '节日', name: '在一起纪念日', year: '2022', date: '11-07',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-11-07' },
-        // 生日
-        { keyword: 'birthday_message', date: '1999-11-26' },
-      ],
-    },
-  ],
-
-  SLOT_LIST: [
-    // 这样配置的话，就会每次发送这句话
-    { keyword: 'note_en', contents: 'I miss you every day!' },
-    // 这样配置的话，就会每次发送这句话
-    { keyword: 'notice', contents: '记得滴眼药水宝宝' },
-    // 这样配置的话，就会每次随机选一句话发送
-    {
-      keyword: 'note_ch',
-      contents: [
-        '因为太喜欢你，所以看谁都像是情敌。',
-        '申请成为你爱里的永久居民。',
-        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
-        '遇见你，就好像捡到了100斤的运气'
       ],
     }
-    
   ],
+
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: false,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: false,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: true,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: false,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },  
+
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: 'b2475209cdfb09f96c347752e6ec4701',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: false,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 1,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+
+  IS_SHOW_COLOR: true,
+
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
@@ -69,11 +121,10 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'obcz76ZjkxdcaN41iXTGBw-lxi3E',
     }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
